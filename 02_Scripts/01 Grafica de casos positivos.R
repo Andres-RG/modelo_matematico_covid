95dded3999d4cf1fb767d6aae9e446f8a5d25bdc
Andres-RG <lrojas26@alumnos.uaq.mx>
2022-09-28 13:13:34 -0500

    Depuración del trabajo

---
+++ 02_Scripts/01 Grafica de casos positivos.R
@@ -0,0 +1,55 @@
+# Librerias necesarias 
+
+library(ggplot2)
+library(ggridges)
+library(tidyverse)
+library(viridisLite)
+library(viridis)
+library(deSolve)
+library(ape)
+library(lubridate)
+
+# Cargar las funciones
+source("02_Scripts/Functions/Functions.R")
+
+# Se carga la base de datos
+load("01_RawData/datos_covid_qro.RData")
+
+# De la base de datos inicial, se determinaran los rangos de edad de todos
+# los casos positivos a Covid-19 hasta diciembre, 2021. A partir de esta 
+# adicion de datos, se construye la grafica para casos positivos divididos
+# por rangos de edades. 
+
+# Este objeto contendra todos los casos, con la columna de rango de edades, y
+# clasificados por estos mismos.
+casos_totales_re <- mutate(datos_covid_qro, rango_de_edad = rangos_edades(datos_covid_qro$EDAD))
+
+# Se guarda el objeto como un objeto .RData
+# saveRDS(casos_totales_re, "03_Out/OutData/casos_totales_rangos_edades.RData")
+
+# Este objeto contendra los casos unicamente positivos, de acuerdo a la clasificacion
+# reportada.  
+casos_positivos_re <- filter(casos_totales_re, CLASIFICACION_FINAL == 1 | 
+                        CLASIFICACION_FINAL == 2 |
+                        CLASIFICACION_FINAL == 3 )
+
+# Se guarda el objeto como un objeto .RData
+# saveRDS(casos_positivos_re, "03_Out/OutData/casos_positivos_rangos_edades.RData")
+
+# Se elabora la grafica de casos positivos separados por rango de edades
+plot_positivos_re <- ggplot(casos_positivos_re, 
+                            aes(x=FECHA_SINTOMAS, y=EDAD, fill = rango_de_edad)) + 
+    geom_bar(position="stack", stat="identity") + 
+    ggtitle("Casos positivos a COVID por rangos de edades 
+          para el estado de Queretaro") + 
+    labs(x="Tiempo", y="Casos") +
+    labs(fill="Rangos de Edad") +
+    theme(plot.title = element_text(hjust = 0.5))+
+    theme(panel.background = element_rect(fill = "white"), 
+          axis.line = element_line(colour = "black", size = 1)) +
+    scale_fill_viridis(discrete = T)
+
+# El objeto que contiene la grafica se guarda como una imagen png
+png("03_Out/Plots/Casos positivos a COVID por rangos de edades para el estado de Queretaro.png", width = 650,  height = 350)
+plot_positivos_re
+dev.off()
---
+++ 02_Scripts/Functions/Functions.R
@@ -0,0 +1,115 @@
+# Función para establecer los rangos de edades
+
+rangos_edades <- function (colm) {
+  
+  re <- c()
+  
+  for (i in 1:length(colm)) {
+    
+    if (colm[[i]] >= 18 & colm[[i]] <= 29) {
+      re <- c(re, "18-29")
+    } else if (colm[[i]] >= 30 & colm[[i]] <= 39) {
+      re <- c(re, "30-39")
+    } else if (colm[[i]] >= 40 & colm[[i]] <= 49) {
+      re <- c(re, "40-49")
+    } else if (colm[[i]] >= 50 & colm[[i]] <= 59) {
+      re <- c(re, "50-59")
+    } else if (colm[[i]] >= 60 & colm[[i]] <= 69) {
+      re <- c(re, "60-69")
+    } else if (colm[[i]] >= 70) {
+      re <- c(re, "70+")
+    } else if (colm[[i]] < 18) {
+      re <- c(re, "18-")
+    }
+  }
+  
+  re <- as.vector(re)
+  
+  return(re)
+  
+}
+
+
+
+
+
+
+
+
+
+
+# Funcion para determinar las probabilidades de trancion
+
+probabilidades <- function(base_1, base_2){
+  
+  ##
+  p18 <- sum(base_1$rango_edad == "18-") / sum(base_2$rango_edad == "18-")
+  ##
+  ##
+  p18_29 <- sum(base_1$rango_edad == "18-29") / 
+    sum(base_2$rango_edad == "18-29")
+  ##
+  ##
+  p30_39 <- sum(base_1$rango_edad == "30-39") / 
+    sum(base_2$rango_edad == "30-39")
+  ##
+  ##
+  p40_49 <- sum(base_1$rango_edad == "40-49") /
+    sum(base_2$rango_edad == "40-49")
+  ##
+  ##
+  p50_59 <- sum(base_1$rango_edad == "50-59") /
+    sum(base_2$rango_edad == "50-59")
+  ##
+  ##
+  p60_69 <- sum(base_1$rango_edad == "60-69") /
+    sum(base_2$rango_edad == "60-69")
+  ##
+  ##
+  p70 <- sum(base_1$rango_edad == "70+") /
+    sum(base_2$rango_edad == "70+")
+  
+  
+  
+  tabla_prob <- rbind(p18, p18_29, p30_39, p40_49, p50_59, p60_69, p70)
+  
+  return(tabla_prob)
+  
+}
+
+
+
+
+
+
+
+
+
+
+# Funcion para establecer las fechas y periodos de vacunacion
+
+fechas_vacunacion <- function(fecha){
+  
+  vac <- c()
+  
+  for (i in fecha) {
+    
+    if( i >= as.Date("2020-12-01") & i <= as.Date("2021-01-31") ) {
+      vac <- c(vac, "Primera Fase")
+    } else if ( i >= as.Date("2021-02-01") & i <= as.Date("2021-04-30") ) {
+      vac <- c(vac, "Segunda Fase")
+    } else if ( i >= as.Date("2021-05-01") & i <= as.Date("2021-05-31") ) {
+      vac <- c(vac, "Tercera Fase")
+    } else if ( i >= as.Date("2021-06-01") & i <= as.Date("2021-06-30") ) {
+      vac <- c(vac, "Cuarta Fase")
+    } else if ( i >= as.Date("2021-07-01") & i <= as.Date("2027-04-07") ) {
+      vac <- c(vac, "Quinta Fase")
+    } else {
+      vac <- c(vac, "NA")
+    }
+  }
+  
+  vac <- as.vector(vac)
+  
+  return(vac)
+}
