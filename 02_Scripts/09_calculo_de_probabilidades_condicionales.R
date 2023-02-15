bb4e4d6f9d42675b33b6de9e49120fdbbea4035a
Andres-RG <lrojas26@alumnos.uaq.mx>
2023-02-15 16:32:32 -0600

    se eliminaron columnas a tomar en cuenta para la determinación de probabilidades

--- 02_Scripts/09_calculo_de_probabilidades_condicionales.R
+++ 02_Scripts/09_calculo_de_probabilidades_condicionales.R
@@ -28,48 +28,45 @@ source("02_Scripts/Functions/Functions.R")
 # 1 Seleccionar solo las columnas de interes 
 casos_pos_re_comorbilidad <- select(casos_positivos_re, 
                                     c(FECHA_SINTOMAS, EDAD, INTUBADO, NEUMONIA,
-                                      DIABETES, EPOC, ASMA, INMUSUPR, HIPERTENSION,
-                                      OTRA_COM, CARDIOVASCULAR, OBESIDAD, RENAL_CRONICA, 
-                                      TABAQUISMO, OTRO_CASO, rango_de_edad))
+                                      DIABETES, EPOC, ASMA, INMUSUPR, HIPERTENSION, 
+                                      CARDIOVASCULAR, OBESIDAD, RENAL_CRONICA, 
+                                      TABAQUISMO, rango_de_edad))
 casos_pos_re_comorbilidad <- arrange(casos_pos_re_comorbilidad, rango_de_edad)
 # save(casos_pos_re_comorbilidad, file = "03_Out/OutData/casos_positivos_re_comorbilidad.RData")
 
 # Ordenar por rango de edad
-comorbilidades_casos <- casos_pos_re_comorbilidad[,c(5:15)]
+comorbilidades_casos <- casos_pos_re_comorbilidad[,c(5:13)]
 
 # cat 1. Menores de 18 años
 casos_pos_re_comorbilidad_cat_1 <- filter(casos_pos_re_comorbilidad, 
                                           rango_de_edad == "18-")
-comorbilidades_casos_1 <- casos_pos_re_comorbilidad_cat_1[,c(5:15)]
+comorbilidades_casos_1 <- casos_pos_re_comorbilidad_cat_1[,c(5:13)]
 comorbilidades_casos_1 <- t(comorbilidades_casos_1)
 
 
 # cat 2. 18 a 39 años
 casos_pos_re_comorbilidad_cat_2 <- filter(casos_pos_re_comorbilidad, 
                                           rango_de_edad == c("18-29", "30-39"))
-comorbilidades_casos_2 <- casos_pos_re_comorbilidad_cat_2[,c(5:15)]
+comorbilidades_casos_2 <- casos_pos_re_comorbilidad_cat_2[,c(5:13)]
 comorbilidades_casos_2 <- t(comorbilidades_casos_2)
 
 
 # cat 3. 40 a 59 años
 casos_pos_re_comorbilidad_cat_3 <- filter(casos_pos_re_comorbilidad, 
                                           rango_de_edad == c("40-49", "50-59"))
-comorbilidades_casos_3 <- casos_pos_re_comorbilidad_cat_3[,c(5:15)]
+comorbilidades_casos_3 <- casos_pos_re_comorbilidad_cat_3[,c(5:13)]
 comorbilidades_casos_3 <- t(comorbilidades_casos_3)
 
 
 # cat 4. 60 años en adelante
 casos_pos_re_comorbilidad_cat_4 <- filter(casos_pos_re_comorbilidad, 
                                           rango_de_edad == c("60-69", "70+"))
-comorbilidades_casos_4 <- casos_pos_re_comorbilidad_cat_4[,c(5:15)]
+comorbilidades_casos_4 <- casos_pos_re_comorbilidad_cat_4[,c(5:13)]
 comorbilidades_casos_4 <- t(comorbilidades_casos_4)
 
 # 2 Determinacion de probabilidad
-# p( cat_1 | com ) = (SUM com / N) / P( cat_1 )
+# p( cat_i | com_j ) = ( p ( com_j | cat_1 ) * p( com_j ) ) / (.p( com_j ) * p ( com_j | cat_1 ) + (1-p( com_j )) * (1-p ( com_j | cat_1 )) )
 
-## 2 . 1   p ( com n cat ) 
-
-comorbilidades <- comorbilidadesdet(comorbilidades_casos_1)
 
 
 
