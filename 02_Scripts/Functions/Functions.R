b3f54d7b6b45fafa1cac6ddd59e78bb7a8bc5c8e
Andres-RG <lrojas26@alumnos.uaq.mx>
2023-02-22 14:37:45 -0600

    Comentarios de las funciones

--- 02_Scripts/Functions/Functions.R
+++ 02_Scripts/Functions/Functions.R
@@ -1,45 +1,61 @@
-# Función para establecer los rangos de edades
-
+# 1. Función para establecer los rangos de edades ====
+# Toma como argumento una columna de la base de datos
+# En este caso siempre va a ser la edad de los casos
 rangos_edades <- function (colm) {
   
+  # Genera un vector vacío
   re <- c()
   
+  # Establece un ciclo for, en donde, por cada variable i en 1 hasta la longitud
+  # de la columna:
   for (i in 1:length(colm)) {
     
+    # Si la variable i en la columna de edad es mayor o igual a 18 Y menor o 
+    # igual a 29 ...
     if (colm[[i]] >= 18 & colm[[i]] <= 29) {
-      re <- c(re, "18-29")
-    } else if (colm[[i]] >= 30 & colm[[i]] <= 39) {
-      re <- c(re, "30-39")
-    } else if (colm[[i]] >= 40 & colm[[i]] <= 49) {
-      re <- c(re, "40-49")
-    } else if (colm[[i]] >= 50 & colm[[i]] <= 59) {
-      re <- c(re, "50-59")
-    } else if (colm[[i]] >= 60 & colm[[i]] <= 69) {
-      re <- c(re, "60-69")
-    } else if (colm[[i]] >= 70) {
-      re <- c(re, "70+")
-    } else if (colm[[i]] < 18) {
-      re <- c(re, "18-")
+      re <- c(re, "18-29") #... colocará en el vector el rango de edad "18-29"
+    } else if (colm[[i]] >= 30 & colm[[i]] <= 39) { # Si la variable i en la 
+                                                    # columna de edad es mayor 
+                                                    # o igual a 30 Y menor o 
+                                                    # igual a 39 ...
+      re <- c(re, "30-39") #... colocará en el vector el rango de edad "30-39"
+    } else if (colm[[i]] >= 40 & colm[[i]] <= 49) { # Si la variable i en la 
+                                                    # columna de edad es mayor 
+                                                    # o igual a 40 Y menor o 
+                                                    # igual a 49 ...
+      re <- c(re, "40-49") #... colocará en el vector el rango de edad "40-49"
+    } else if (colm[[i]] >= 50 & colm[[i]] <= 59) { # Si la variable i en la 
+                                                    # columna de edad es mayor 
+                                                    # o igual a 50 Y menor o 
+                                                    # igual a 59 ...
+      re <- c(re, "50-59") #... colocará en el vector el rango de edad "50-59"
+    } else if (colm[[i]] >= 60 & colm[[i]] <= 69) { # Si la variable i en la 
+                                                    # columna de edad es mayor 
+                                                    # o igual a 60 Y menor o 
+                                                    # igual a 69 ...
+      re <- c(re, "60-69") #... colocará en el vector el rango de edad "60-69"
+    } else if (colm[[i]] >= 70) { # Si la variable i en la columna de edad es 
+                                  # mayor o igual a 70...
+      re <- c(re, "70+") #... colocará en el vector el rango de edad "70+"
+    } else if (colm[[i]] < 18) { # Si la variable i en la columna de edad es 
+                                 # menor a 18...
+      re <- c(re, "18-") #... colocará en el vector el rango de edad "18-"
     }
   }
   
+  # Convierte el vector con los datos en un objeto tipo vector
   re <- as.vector(re)
-  
+  # Regresa el contenido del vector
   return(re)
   
 }
 
 
 
-
-
-
-
-
-
-
-# Funcion para determinar las probabilidades de transicion
-
+# 2. Funcion para determinar las probabilidades de transicion ====
+#    Toma como argumento dos bases de datos. Esta funcion sera utilizada para 
+#    las bases de datos que ya esten filtradas. # Toma los casos de la base 1 
+#    y se dividen entre el total de los casos de la base 2, por rango de edad. 
 probabilidades <- function(base_1, base_2){
   
   ##
@@ -70,32 +86,31 @@ probabilidades <- function(base_1, base_2){
     sum(base_2$rango_de_edad == "70+")
   
   
-  
+  # Genera una tabla con todas las probabilidades de todos los rangos de edad
   tabla_prob <- rbind(p18, p18_29, p30_39, p40_49, p50_59, p60_69, p70)
-  
+  # Regresa la tabla
   return(tabla_prob)
   
 }
 
 
 
-
-
-
-
-
-
-
-# Funcion para establecer las fechas y periodos de vacunacion
-
+# 3. Funcion para establecer las fechas y periodos de vacunacion ====
+#   Toma como argumento la fecha, que corresponde a la fecha de inicio de 
+#   sintomas de los datos de covid.
 fechas_vacunacion <- function(fecha){
   
+  # Genera un vector vacio 
   vac <- c()
   
+  # Establece un ciclo for, donde va a iterar la variable i en la columna de 
+  # las fechas donde: 
   for (i in fecha) {
     
-    if( i >= as.Date("2020-12-01") & i <= as.Date("2021-01-31") ) {
-      vac <- c(vac, "Primera Fase")
+    if( i >= as.Date("2020-12-01") & i <= as.Date("2021-01-31") ) { 
+      # Si la fecha esta entre un rango de fechas especificas ...
+      vac <- c(vac, "Primera Fase") # ... corresponde a un periodo de vacunacion
+                                    # y lo colocara en el vector
     } else if ( i >= as.Date("2021-02-01") & i <= as.Date("2021-04-30") ) {
       vac <- c(vac, "Segunda Fase")
     } else if ( i >= as.Date("2021-05-01") & i <= as.Date("2021-05-31") ) {
@@ -104,53 +119,53 @@ fechas_vacunacion <- function(fecha){
       vac <- c(vac, "Cuarta Fase")
     } else if ( i >= as.Date("2021-07-01") & i <= as.Date("2027-04-07") ) {
       vac <- c(vac, "Quinta Fase")
-    } else {
-      vac <- c(vac, "NA")
+    } else { # En cambio, todas las fechas que no esten incluidas en los 
+             # periodos de vacunacion ... 
+      vac <- c(vac, "NA") # Colocara una NA en el vector
     }
   }
   
+  # Convierte el vector de las fechas en un objeto tipo vector
   vac <- as.vector(vac)
-  
+  # Regresa el objeto
   return(vac)
 }
 
 
 
-
-
-
-
-
-
-
-# Funcion para determinar la presencia de comorbilidades 
-# 1 = SI ; 0 = NO 
+# 4. Funcion para determinar la presencia de comorbilidades ====
+#   1 = SI ; 0 = NO 
+#   Toma como argumento la base de datos
 comorbilidadesdet <- function( base ){
+  
+  # Genera un vector vacio
   comorbilidades <- c()
-  ##
+  # Genera un vector con una secuencia de 1 hasta el numero de columnas de la 
+  # base de datos
   n <- seq(1:ncol(base))
+  # Establece un ciclo for, donde para cada variable i en el vector de numeros 
+  # de la secuencia N:
   for (i in n) {
     if ( any(base[,i] == 1)){
-      comorbilidades <- c(comorbilidades,1)
-    } else {
-      comorbilidades <- c(comorbilidades,0)
+      # Si hay alguna variable, que sea exactamente igual a 1 en la columna i, 
+      # que corresponde a cada caso, indicaria la presencia de esa comorbilidad
+      comorbilidades <- c(comorbilidades,1) # coloca entonces, un 1 en el vector
+    } else { # En cambio ...
+      comorbilidades <- c(comorbilidades,0) # coloca un 0 en el vector
     }
   }
-  ##
+  #Regresa el vector
   return(comorbilidades)
 }
 
 
 
-
-
-
-
-# FUNCION PARA CONTAR CUANTOS PACIENTES TIENEN COMORBILIDADES ESPECIFICAS
-# DE LA BASE DE DATOS DE COVID
+# 5. Funcion para el conteo de comorbilidades de cada caso ====
+#    Toma como argmuento la base de datos de covid. 
 comorbilidades_conteo <- function(base){
   
-  #contadores vacios
+  # Genera los contadores vacios para cada una de las comorbilidades, 
+  # con valor de 0
   diabetes_conteo         <- 0
   epoc_conteo             <- 0
   asma_conteo             <- 0
@@ -161,15 +176,20 @@ comorbilidades_conteo <- function(base){
   renal_cronica_conteo    <- 0
   tabaquismo_conteo       <- 0
   
-  # para cada columna de la base
-  for (i in colnames(base[,5:13])){
-    if (i == "DIABETES"){
-      for (n in base[[i]]){
-        if (n != 2){
-          diabetes_conteo <- diabetes_conteo + 1
+  # Establece un ciclo for, donde va a iterar las columnas de la base de datos 
+  # con la variable i:
+  for (i in colnames(base[,5:13])){ 
+    if (i == "DIABETES"){ # Si la variable i es exactamente igual a 
+                          # a la comorbilidad especifica ...
+      for (n in base[[i]]){ # Establece un segundo ciclo for, donde para cada 
+                            # valor de la columna de la comorbilidad:
+        if (n != 2){ # Si el valor es diferente a 2 ....
+          diabetes_conteo <- diabetes_conteo + 1 # Al contador de la 
+                                                 # comorbilidad en especifico, 
+                                                 # le suma 1.
         }
-      }
-    } else if( i == "EPOC"){
+      } # Cierra el ciclo cuando ya recorrio toda la columna 
+    } else if( i == "EPOC"){ # En cambio, pasa a la siguiente comorbilidad
       for (n in base[[i]]){
         if (n != 2){
           epoc_conteo <- epoc_conteo + 1
@@ -220,6 +240,8 @@ comorbilidades_conteo <- function(base){
     } 
   }
   
+  # Genera un vector con todos los contadores una vez recorrio toda la base
+  # de datos
   com_conteos <- c(diabetes_conteo         ,
                    epoc_conteo             ,
                    asma_conteo             ,
@@ -235,10 +257,11 @@ comorbilidades_conteo <- function(base){
 
 
 
-
-
-# FUNCION PARA CONTAR CUANTOS PACIENTES ****NO**** TIENEN 
-# COMORBILIDADES ESPECIFICAS DE LA BASE DE DATOS DE COVID
+# 6. Funcion para el conteo de casos que *NO TIENEN* comorbilidades  ====
+#    Se basa en la misma estructura que la funcion anterior, solo que para este 
+#    caso, el condicional IF que define la presencia de comorbilidad, es que sea
+#    exactamente igual a 2, es decir, de acuerdo a los datos de covid, que sea
+#    NO
 sin_comorbilidades_conteo <- function(base){
   
   #contadores vacios
