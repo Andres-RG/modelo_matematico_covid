943a13d03d2aeb51e1cac7413a002dc56cd7c856
Andres-RG <lrojas26@alumnos.uaq.mx>
2023-02-23 14:12:18 -0600

    Se corrigieron las probabilidades condicionales. Se obtuvo p(cat_i | com_j). Hay probabilidades que son mayores que 1

--- 02_Scripts/09_calculo_de_probabilidade_condicionales_p(cat_com).R
+++ 02_Scripts/09_calculo_de_probabilidade_condicionales_p(cat_com).R
@@ -12,6 +12,7 @@ library(ggmatplot)
 library(randomcoloR)
 
 # Se carga la base de datos
+load("03_Out/OutData/casos_totales_rangos_edades.RData")
 load("03_Out/OutData/casos_positivos_rangos_edades.RData")
 load("03_Out/OutData/casos_positivos_re_comorbilidad.RData")
 load("03_Out/OutData/conteo_comorbilidades.RData")
@@ -29,7 +30,7 @@ source("02_Scripts/Functions/Functions.R")
 #  es más probable que  pertenezcan
 #  p( cat_j | com_i )
 
-# 1 Ajustar los datos ----
+# 1 Ajustar los datos -------------------------------------------
 # 1.1 Seleccionar solo las columnas de interes con comorbilidades
 casos_pos_re_comorbilidad <- select(casos_positivos_re, 
                                     c(FECHA_SINTOMAS, EDAD, INTUBADO, NEUMONIA,
@@ -64,22 +65,25 @@ casos_pos_re_comorbilidad_cat_4 <- filter(casos_pos_re_comorbilidad,
                                           rango_de_edad == "60-69" | rango_de_edad == "70+")
 comorbilidades_casos_4 <- t(comorbilidades_casos_4)
 
-# 2 Determinacion de probabilidad ----
-# p( cat_i | com_j ) = ( p ( com_j | cat_1 ) * p( com_j ) ) / 
-#                      (.p( com_j ) * p ( com_j | cat_1 ) + (1-p( com_j )) * 
-#                      (1-p ( com_j | cat_1 )) )
 
-# 2.1 Obtención de p( com_j | cat_i ) ----
+
+
+# 2 Determinacion de probabilidad -------------------------------
+# p( cat_i | com_j ) = P ( COM_j & CAT_i )
+#                      --- --- --- --- ---
+#                         P ( COM_j )
+# 2.1 ObtenciOn de P ( COM_j & CAT_i )
+#.    # casos de comorbilidad de la cat_i
+#.    --- --- --- --- --- --- --- --- ---
+#.            # casos de la cat_i
 # 2.1.1 Conteo de casos con comorbilidades ----
-
 # 2.1.1.1 Se obtienen los conteos por categoria de cuantos pacientes tienen alguna
 #         comorbilidad, se hace por cada una, y para cada categoria.
-#         *Se hace uso de una funcion.
+#         * Se hace uso de una funcion.
 com_cat_1 <- comorbilidades_conteo(casos_pos_re_comorbilidad_cat_1)
 com_cat_2 <- comorbilidades_conteo(casos_pos_re_comorbilidad_cat_2)
 com_cat_3 <- comorbilidades_conteo(casos_pos_re_comorbilidad_cat_3)
 com_cat_4 <- comorbilidades_conteo(casos_pos_re_comorbilidad_cat_4)
-
 # 2.1.1.2 Se compila la informacion obtenida en una base de datos
 comorbilidades_conteos <- data.frame(com_cat_1,com_cat_2,com_cat_3,com_cat_4)
 rownames(comorbilidades_conteos) <- c("diabetes"         ,
@@ -96,8 +100,7 @@ colnames(comorbilidades_conteos) <- c("categoria 1", "categoria 2",
 comorbilidades_conteos
 # 2.1.1.2 Se guaradan los conteos de las personas con comorbilidades
 # save(comorbilidades_conteos, file = "03_Out/OutData/conteo_comorbilidades.RData")
-
-
+###################################################################################
 # 2.1.1.3 Se hace el mismo procedimiento para pacientes SIN COMORBILIDADES*
 #         Se realizo una funcion similar a la anterior, solo que para este caso
 #         cuenta todos los casos que no tienen alguna comorbilidad
@@ -105,7 +108,6 @@ sin_com_cat_1 <- sin_comorbilidades_conteo(casos_pos_re_comorbilidad_cat_1)
 sin_com_cat_2 <- sin_comorbilidades_conteo(casos_pos_re_comorbilidad_cat_2)
 sin_com_cat_3 <- sin_comorbilidades_conteo(casos_pos_re_comorbilidad_cat_3)
 sin_com_cat_4 <- sin_comorbilidades_conteo(casos_pos_re_comorbilidad_cat_4)
-
 # 2.1.1.4 Se compila la información obtenida en una base de datos
 sin_comorbilidades_conteos <- data.frame(sin_com_cat_1,sin_com_cat_2,
                                          sin_com_cat_3,sin_com_cat_4)
@@ -123,7 +125,7 @@ colnames(sin_comorbilidades_conteos) <- c("categoria 1", "categoria 2",
 sin_comorbilidades_conteos
 # 2.1.1.5 Se guarda la base de datos
 # save(comorbilidades_conteos, file = "03_Out/OutData/conteo_SIN_comorbilidades.RData")
-
+###################################################################################
 
 # 2.1.2 Conteo de individuos por categoria ----
 # 2.1.2.1      CATEGORIA 1: MENORES DE 18 AÑOS
@@ -134,11 +136,8 @@ c2 <- sum(casos_pos_re_comorbilidad_cat_2$ind)
 c3 <- sum(casos_pos_re_comorbilidad_cat_3$ind)
 # 2.1.2.4      CATEOGIRA 4: 60 AÑOS EN ADELANTE
 c4 <- sum(casos_pos_re_comorbilidad_cat_4$ind)
-# 2.1.2.5      TOTAL DE LA POBLACION/CASOS REGISTRADOS
-N <- c1+c2+c3+c4
 
-
-# 2.1.3 Determinación de la probabilidad P ( COM_J | CAT_I )----
+# 2.1.3 Determinación de la probabilidad P ( COM_j & CAT_i )----
 #       Se dividen los casos de comorbilidad, por comorbilidad, de cada 
 #       categoria entre el total de casos de esa categoria
 # 2.1.3.1 ---- CATEGORIA 1 
@@ -164,7 +163,6 @@ p_tabaquismo_c1 <- comorbilidades_conteos[9,1]/c1
 p_comorb_c1 <- c(p_diab_c1,p_epoc_c1,p_asma_c1,p_inmunsupr_c1,
                  p_hipertension_c1,p_cardiovascular_c1,p_obesidad_c1,
                  p_renal_cronica_c1,p_tabaquismo_c1)
-
 
 # 2.1.3.2 ---- CATEGORIA 2
 # 2.1.3.2.1 p( diabetes | c2 )
@@ -240,8 +238,7 @@ p_comorb_c4 <- c(p_diab_c4,p_epoc_c4,p_asma_c4,p_inmunsupr_c4,
                  p_hipertension_c4,p_cardiovascular_c4,p_obesidad_c4,
                  p_renal_cronica_c4,p_tabaquismo_c4)
 
-
-# 2.1.4 Se guarda como una matriz, todas las P ( COM_I | CAT_J ) obtenidas ---- 
+# 2.1.4 Se guarda como una matriz, todas las P ( COM_I & CAT_J ) obtenidas ---- 
 #       previamente
 matriz_comor <- matrix(c(p_comorb_c1,p_comorb_c2,p_comorb_c3,p_comorb_c4),
                        ncol = 4, byrow = F)
@@ -257,61 +254,78 @@ rownames(matriz_comor) <- c("p ( diabetes | c_j )",
 colnames(matriz_comor) <-c("CATEGORIA 1", "CATEGORIA 2", "CATEGORIA 3", "CATEGORIA 4")
 matriz_comor
 
-
-# 2.1.5 Determinacion de P ( COM_J ) ----
-#       = #TODOS LOS QUE TIENEN LA COM_J / N
-# 2.1.5.1 P ( DIABETES )
-p_diabetes <- sum(comorbilidades_conteos[1,])/(N)
-# 2.1.5.2 P ( EPOC )
-p_epoc <- sum(comorbilidades_conteos[2,])/(N)
-# 2.1.5.3 P ( ASMA )
-p_asma <- sum(comorbilidades_conteos[3,])/(N)
-# 2.1.5.4 P ( INMUNSUPR )
-p_inmunsupr <- sum(comorbilidades_conteos[4,])/(N)
-# 2.1.5.5 P ( HIPERTENSION )
-p_hipertension <- sum(comorbilidades_conteos[5,])/(N)
-# 2.1.5.6 P ( CARDIOVASCULAR )
-p_cardiovascular <- sum(comorbilidades_conteos[6,])/(N)
-# 2.1.5.7 P ( OBESIDAD )
-p_obesidad <- sum(comorbilidades_conteos[7,])/(N)
-# 2.1.5.8 P ( RENAL_CRONICA )
-p_renal_cronica <- sum(comorbilidades_conteos[8,])/(N)
-# 2.1.5.9 P ( TABAQUISMO )
-p_tabaquismo <- sum(comorbilidades_conteos[9,])/(N)
+# 2.1.5 Determinacion de P ( COM_j ) ----
+#       =         # TODOS LOS QUE TIENEN LA COM_j
+#.        --- --- --- --- --- --- --- --- --- --- ---
+#.          # Toda la poblacion de la base de datos
+# 2.1.5.1 Conteo de comorbilidades de los casos TOTALES
+# 2.1.5.1.1 Filtrar la base de datos TOTALES
+casos_totales_re_comorbilidad <- select(casos_totales_re, 
+                                        c(FECHA_SINTOMAS, EDAD, INTUBADO, NEUMONIA,
+                                          DIABETES, EPOC, ASMA, INMUSUPR, HIPERTENSION,
+                                          CARDIOVASCULAR, OBESIDAD, RENAL_CRONICA,
+                                          TABAQUISMO, rango_de_edad))
+# 2.1.5.1.2 Se agrega la columna de individuo 1 para sumar
+casos_totales_re_comorbilidad <- mutate(casos_totales_re_comorbilidad, ind = 1)
+# 2.1.5.1.3 Se hace el conteo con la funcion
+com_totales <- comorbilidades_conteo(casos_totales_re_comorbilidad)
+# 2.1.5.1.4 Se nombra a cada elemento del vector resultante con los conteos de casos 
+#           con comorbilidad
+names(com_totales) <- c("diabetes"         ,
+                        "epoc"             ,
+                        "asma"             ,
+                        "inmunsupr"        ,
+                        "hipertension"     ,
+                        "cardiovascular"   ,
+                        "obesidad"         ,
+                        "renal_cronica"    ,
+                        "tabaquismo"       )
+# 2.1.5.2 Conteo de casos totales
+n_total <- sum(casos_totales_re_comorbilidad$ind)
+# 2.1.5.3 Determinacion de la probabilidad
+# 2.1.5.3.1 P ( DIABETES )
+p_diabetes <- com_totales[1]/n_total
+# 2.1.5.3.2 P ( EPOC )
+p_epoc <- com_totales[2]/n_total
+# 2.1.5.3.3 P ( ASMA )
+p_asma <- com_totales[3]/n_total
+# 2.1.5.3.4 P ( INMUNSUPR )
+p_inmunsupr <- com_totales[4]/n_total
+# 2.1.5.3.5 P ( HIPERTENSION )
+p_hipertension <- com_totales[5]/n_total
+# 2.1.5.3.6 P ( CARDIOVASCULAR )
+p_cardiovascular <- com_totales[6]/n_total
+# 2.1.5.3.7 P ( OBESIDAD )
+p_obesidad <- com_totales[7]/n_total
+# 2.1.5.3.8 P ( RENAL_CRONICA )
+p_renal_cronica <- com_totales[8]/n_total
+# 2.1.5.3.9 P ( TABAQUISMO )
+p_tabaquismo <- com_totales[9]/n_total
 
 
 # 2.1.6 Determinacion de las PROBABILIDADES CONDICIONALES ----
-# p( cat_i | com_j ) = ( p ( com_j | cat_1 ) * p( com_j ) ) / 
-#                      (.p( com_j ) * p ( com_j | cat_1 ) + (1-p( com_j )) * 
-#                      (1-p ( com_j | cat_1 )) )
+# p( cat_i | com_j ) = P ( COM_j & CAT_i )
+#                      --- --- --- --- ---
+#                         P ( COM_j )
 # 2.1.6.1 CATEGORIA 1 ===
 # 2.1.6.1.1 p ( C1 | Diabetes ) 
-p_c1_diabetes <-( p_diab_c1 * p_diabetes )/
-    ( p_diabetes * p_diab_c1 + ((1-p_diabetes) * (1-p_diab_c1)))
+p_c1_diabetes <- p_diab_c1 / p_diabetes
 # 2.1.6.1.2 p ( C1 | EPOC ) 
-p_c1_epoc <-( p_epoc_c1 * p_epoc )/
-    ( p_epoc * p_epoc_c1 + ((1-p_epoc) * (1-p_epoc_c1)))
+p_c1_epoc <- p_epoc_c1 / p_epoc
 # 2.1.6.1.3 p ( C1 | ASMA ) 
-p_c1_asma <-( p_asma_c1 * p_asma )/
-    ( p_asma * p_asma_c1 + ((1-p_asma) * (1-p_asma_c1)))
+p_c1_asma <- p_asma_c1 / p_asma
 # 2.1.6.1.4 p ( C1 | INMUNSUPR ) 
-p_c1_inmunsupr <-( p_inmunsupr_c1 * p_inmunsupr )/
-    ( p_inmunsupr * p_inmunsupr_c1 + ((1-p_inmunsupr) * (1-p_inmunsupr_c1)))
+p_c1_inmunsupr <- p_inmunsupr_c1 / p_inmunsupr
 # 2.1.6.1.5 p ( C1 | HIPERTENSION ) 
-p_c1_hipertension <-( p_hipertension_c1 * p_hipertension )/
-    ( p_hipertension * p_hipertension_c1 + ((1-p_hipertension) * (1-p_hipertension_c1)))
+p_c1_hipertension <- p_hipertension_c1 / p_hipertension
 # 2.1.6.1.6 p ( C1 | CARDIOVASCULAR ) 
-p_c1_cardiovascular <-( p_cardiovascular_c1 * p_cardiovascular )/
-    ( p_cardiovascular * p_cardiovascular_c1 + ((1-p_cardiovascular) * (1-p_cardiovascular_c1)))
+p_c1_cardiovascular <-p_cardiovascular_c1 / p_cardiovascular
 # 2.1.6.1.7 p ( C1 | OBESIDAD ) 
-p_c1_obesidad <-( p_obesidad_c1 * p_obesidad )/
-    ( p_obesidad * p_obesidad_c1 + ((1-p_obesidad) * (1-p_obesidad_c1)))
+p_c1_obesidad <- p_obesidad_c1 / p_obesidad
 # 2.1.6.1.8 p ( C1 | RENAL_CRONICA ) 
-p_c1_renal_cronica <-( p_renal_cronica_c1 * p_renal_cronica )/
-    ( p_renal_cronica * p_renal_cronica_c1 + ((1-p_renal_cronica) * (1-p_renal_cronica_c1)))
+p_c1_renal_cronica <- p_renal_cronica_c1 / p_renal_cronica
 # 2.1.6.1.9 p ( C1 | TABAQUISMO ) 
-p_c1_tabaquismo <-( p_tabaquismo_c1 * p_tabaquismo )/
-    ( p_tabaquismo * p_tabaquismo_c1 + ((1-p_tabaquismo) * (1-p_tabaquismo_c1)))
+p_c1_tabaquismo <- p_tabaquismo_c1 / p_tabaquismo
 # 2.1.6.1.10 Se guardan todas las p´s en un vector
 p_c1_com <- c(p_c1_diabetes,p_c1_epoc,p_c1_asma,p_c1_inmunsupr,
               p_c1_hipertension,p_c1_cardiovascular,p_c1_obesidad,
@@ -319,33 +333,24 @@ p_c1_com <- c(p_c1_diabetes,p_c1_epoc,p_c1_asma,p_c1_inmunsupr,
 
 
 # 2.1.6.2 CATEGORIA 2 ===
-# 2.1.6.2.1 p ( C2 | Diabetes ) 
-p_c2_diabetes <-( p_diab_c2 * p_diabetes )/
-    ( p_diabetes * p_diab_c2 + ((1-p_diabetes) * (1-p_diab_c2)))
-# 2.1.6.2.2 p ( C2 | EPOC ) 
-p_c2_epoc <-( p_epoc_c2 * p_epoc )/
-    ( p_epoc * p_epoc_c2 + ((1-p_epoc) * (1-p_epoc_c2)))
-# 2.1.6.2.3 p ( C2 | ASMA ) 
-p_c2_asma <-( p_asma_c2 * p_asma )/
-    ( p_asma * p_asma_c2 + ((1-p_asma) * (1-p_asma_c2)))
+# 2.1.6.2.1 p ( C2 | Diabetes )
+p_c2_diabetes <- p_diab_c2 / p_diabetes
+# 2.1.6.2.2 p ( C2 | EPOC )
+p_c2_epoc <- p_epoc_c2 / p_epoc
+# 2.1.6.2.3 p ( C2 | ASMA )
+p_c2_asma <- p_asma_c2 / p_asma
 # 2.1.6.2.4 p ( C2 | INMUNSUPR ) 
-p_c2_inmunsupr <-( p_inmunsupr_c2 * p_inmunsupr )/
-    ( p_inmunsupr * p_inmunsupr_c2 + ((1-p_inmunsupr) * (1-p_inmunsupr_c2)))
+p_c2_inmunsupr <- p_inmunsupr_c2 / p_inmunsupr
 # 2.1.6.2.5 p ( C2 | HIPERTENSION ) 
-p_c2_hipertension <-( p_hipertension_c2 * p_hipertension )/
-    ( p_hipertension * p_hipertension_c2 + ((1-p_hipertension) * (1-p_hipertension_c2)))
+p_c2_hipertension <- p_hipertension_c2 / p_hipertension
 # 2.1.6.2.6 p ( C2 | CARDIOVASCULAR ) 
-p_c2_cardiovascular <-( p_cardiovascular_c2 * p_cardiovascular )/
-    ( p_cardiovascular * p_cardiovascular_c2 + ((1-p_cardiovascular) * (1-p_cardiovascular_c2)))
+p_c2_cardiovascular <- p_cardiovascular_c2 / p_cardiovascular
 # 2.1.6.2.7 p ( C2 | OBESIDAD ) 
-p_c2_obesidad <-( p_obesidad_c2 * p_obesidad )/
-    ( p_obesidad * p_obesidad_c2 + ((1-p_obesidad) * (1-p_obesidad_c2)))
+p_c2_obesidad <- p_obesidad_c2 / p_obesidad
 # 2.1.6.2.8 p ( C2 | RENAL_CRONICA ) 
-p_c2_renal_cronica <-( p_renal_cronica_c2 * p_renal_cronica )/
-    ( p_renal_cronica * p_renal_cronica_c2 + ((1-p_renal_cronica) * (1-p_renal_cronica_c2)))
+p_c2_renal_cronica <- p_renal_cronica_c2 / p_renal_cronica
 # 2.1.6.2.9 p ( C2 | TABAQUISMO ) 
-p_c2_tabaquismo <-( p_tabaquismo_c2 * p_tabaquismo )/
-    ( p_tabaquismo * p_tabaquismo_c2 + ((1-p_tabaquismo) * (1-p_tabaquismo_c2)))
+p_c2_tabaquismo <- p_tabaquismo_c2 / p_tabaquismo
 # 2.1.6.2.10 Se guardan todas las p´s en un vector
 p_c2_com <- c(p_c2_diabetes,p_c2_epoc,p_c2_asma,p_c2_inmunsupr,
               p_c2_hipertension,p_c2_cardiovascular,p_c2_obesidad,
@@ -353,33 +358,24 @@ p_c2_com <- c(p_c2_diabetes,p_c2_epoc,p_c2_asma,p_c2_inmunsupr,
 
 
 # 2.1.6.3 CATEGORIA 3 ===
-# 2.1.6.3.1 p ( C3 | Diabetes ) 
-p_c3_diabetes <-( p_diab_c3 * p_diabetes )/
-    ( p_diabetes * p_diab_c3 + ((1-p_diabetes) * (1-p_diab_c3)))
-# 2.1.6.3.2 p ( C3 | EPOC ) 
-p_c3_epoc <-( p_epoc_c3 * p_epoc )/
-    ( p_epoc * p_epoc_c3 + ((1-p_epoc) * (1-p_epoc_c3)))
-# 2.1.6.3.3 p ( C3 | ASMA ) 
-p_c3_asma <-( p_asma_c3 * p_asma )/
-    ( p_asma * p_asma_c3 + ((1-p_asma) * (1-p_asma_c3)))
+# 2.1.6.3.1 p ( C3 | Diabetes )
+p_c3_diabetes <- p_diab_c3 / p_diabetes
+# 2.1.6.3.2 p ( C3 | EPOC )
+p_c3_epoc <- p_epoc_c3 / p_epoc
+# 2.1.6.3.3 p ( C3 | ASMA )
+p_c3_asma <- p_asma_c3 / p_asma
 # 2.1.6.3.4 p ( C3 | INMUNSUPR ) 
-p_c3_inmunsupr <-( p_inmunsupr_c3 * p_inmunsupr )/
-    ( p_inmunsupr * p_inmunsupr_c3 + ((1-p_inmunsupr) * (1-p_inmunsupr_c3)))
+p_c3_inmunsupr <- p_inmunsupr_c3 / p_inmunsupr
 # 2.1.6.3.5 p ( C3 | HIPERTENSION ) 
-p_c3_hipertension <-( p_hipertension_c3 * p_hipertension )/
-    ( p_hipertension * p_hipertension_c3 + ((1-p_hipertension) * (1-p_hipertension_c3)))
+p_c3_hipertension <- p_hipertension_c3 / p_hipertension
 # 2.1.6.3.6 p ( C3 | CARDIOVASCULAR ) 
-p_c3_cardiovascular <-( p_cardiovascular_c3 * p_cardiovascular )/
-    ( p_cardiovascular * p_cardiovascular_c3 + ((1-p_cardiovascular) * (1-p_cardiovascular_c3)))
+p_c3_cardiovascular <- p_cardiovascular_c3 / p_cardiovascular
 # 2.1.6.3.7 p ( C3 | OBESIDAD ) 
-p_c3_obesidad <-( p_obesidad_c3 * p_obesidad )/
-    ( p_obesidad * p_obesidad_c3 + ((1-p_obesidad) * (1-p_obesidad_c3)))
+p_c3_obesidad <- p_obesidad_c3 / p_obesidad
 # 2.1.6.3.8 p ( C3 | RENAL_CRONICA ) 
-p_c3_renal_cronica <-( p_renal_cronica_c3 * p_renal_cronica )/
-    ( p_renal_cronica * p_renal_cronica_c3 + ((1-p_renal_cronica) * (1-p_renal_cronica_c3)))
+p_c3_renal_cronica <- p_renal_cronica_c3 / p_renal_cronica
 # 2.1.6.3.9 p ( C3 | TABAQUISMO ) 
-p_c3_tabaquismo <-( p_tabaquismo_c3 * p_tabaquismo )/
-    ( p_tabaquismo * p_tabaquismo_c3 + ((1-p_tabaquismo) * (1-p_tabaquismo_c3)))
+p_c3_tabaquismo <- p_tabaquismo_c3 / p_tabaquismo
 # 2.1.6.3.10 Se guardan todas las p´s en un vector
 p_c3_com <- c(p_c3_diabetes,p_c3_epoc,p_c3_asma,p_c3_inmunsupr,
               p_c3_hipertension,p_c3_cardiovascular,p_c3_obesidad,
@@ -387,33 +383,24 @@ p_c3_com <- c(p_c3_diabetes,p_c3_epoc,p_c3_asma,p_c3_inmunsupr,
 
 
 # 2.1.6.4 CATEGORIA 3 ===
-# 2.1.6.4.1 p ( C4 | Diabetes ) 
-p_c4_diabetes <-( p_diab_c4 * p_diabetes )/
-    ( p_diabetes * p_diab_c4 + ((1-p_diabetes) * (1-p_diab_c4)))
-# 2.1.6.4.2 p ( C4 | EPOC ) 
-p_c4_epoc <-( p_epoc_c4 * p_epoc )/
-    ( p_epoc * p_epoc_c4 + ((1-p_epoc) * (1-p_epoc_c4)))
-# 2.1.6.4.3 p ( C4 | ASMA ) 
-p_c4_asma <-( p_asma_c4 * p_asma )/
-    ( p_asma * p_asma_c4 + ((1-p_asma) * (1-p_asma_c4)))
+# 2.1.6.4.1 p ( C4 | Diabetes )
+p_c4_diabetes <- p_diab_c4 / p_diabetes
+# 2.1.6.4.2 p ( C4 | EPOC )
+p_c4_epoc <- p_epoc_c4 / p_epoc
+# 2.1.6.4.3 p ( C4 | ASMA )
+p_c4_asma <- p_asma_c4 / p_asma
 # 2.1.6.4.4 p ( C4 | INMUNSUPR ) 
-p_c4_inmunsupr <-( p_inmunsupr_c4 * p_inmunsupr )/
-    ( p_inmunsupr * p_inmunsupr_c4 + ((1-p_inmunsupr) * (1-p_inmunsupr_c4)))
+p_c4_inmunsupr <- p_inmunsupr_c4 / p_inmunsupr
 # 2.1.6.4.5 p ( C4 | HIPERTENSION ) 
-p_c4_hipertension <-( p_hipertension_c4 * p_hipertension )/
-    ( p_hipertension * p_hipertension_c4 + ((1-p_hipertension) * (1-p_hipertension_c4)))
+p_c4_hipertension <- p_hipertension_c4 / p_hipertension
 # 2.1.6.4.6 p ( C4 | CARDIOVASCULAR ) 
-p_c4_cardiovascular <-( p_cardiovascular_c4 * p_cardiovascular )/
-    ( p_cardiovascular * p_cardiovascular_c4 + ((1-p_cardiovascular) * (1-p_cardiovascular_c4)))
+p_c4_cardiovascular <- p_cardiovascular_c4 / p_cardiovascular
 # 2.1.6.4.7 p ( C4 | OBESIDAD ) 
-p_c4_obesidad <-( p_obesidad_c4 * p_obesidad )/
-    ( p_obesidad * p_obesidad_c4 + ((1-p_obesidad) * (1-p_obesidad_c4)))
+p_c4_obesidad <- p_obesidad_c4 / p_obesidad
 # 2.1.6.4.8 p ( C4 | RENAL_CRONICA ) 
-p_c4_renal_cronica <-( p_renal_cronica_c4 * p_renal_cronica )/
-    ( p_renal_cronica * p_renal_cronica_c4 + ((1-p_renal_cronica) * (1-p_renal_cronica_c4)))
+p_c4_renal_cronica <- p_renal_cronica_c4 / p_renal_cronica
 # 2.1.6.4.9 p ( C4 | TABAQUISMO ) 
-p_c4_tabaquismo <-( p_tabaquismo_c4 * p_tabaquismo )/
-    ( p_tabaquismo * p_tabaquismo_c4 + ((1-p_tabaquismo) * (1-p_tabaquismo_c4)))
+p_c4_tabaquismo <- p_tabaquismo_c4 / p_tabaquismo
 # 2.1.6.4.10 Se guardan todas las p´s en un vector
 p_c4_com <- c(p_c4_diabetes,p_c4_epoc,p_c4_asma,p_c4_inmunsupr,
               p_c4_hipertension,p_c4_cardiovascular,p_c4_obesidad,
