3d1af253e2a278c92eafe644317443da6d83f8d1
Andres-RG <lrojas26@alumnos.uaq.mx>
2023-02-28 14:25:52 -0600

    Calculo de probabilidades condicionales con comorbilidades combinadas

---
+++ 02_Scripts/10_calculo_de_probabilidades_condicionales_con_combinacion_de_comorbilidades.R
@@ -0,0 +1,24 @@
+# Librerias necesarias 
+
+library(ggplot2)
+library(ggridges)
+library(tidyverse)
+library(viridisLite)
+library(viridis)
+library(deSolve)
+library(ape)
+library(lubridate)
+library(ggmatplot)
+library(randomcoloR)
+
+# Se carga la base de datos
+load("03_Out/OutData/casos_totales_rangos_edades.RData")
+load("03_Out/OutData/casos_positivos_rangos_edades.RData")
+load("03_Out/OutData/casos_positivos_re_comorbilidad.RData")
+load("03_Out/OutData/conteo_comorbilidades.RData")
+load("03_Out/OutData/conteo_SIN_comorbilidades.RData")
+load("03_Out/OutData/matriz_p_comorbilidades.RData")
+load("03_Out/OutData/matriz_2_p_comorbilidades.RData")
+
+# Se cargan las funciones
+source("02_Scripts/Functions/Functions.R")
