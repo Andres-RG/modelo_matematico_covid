1ddadd1544eb85612645c82e666e0905165dfb95
Andres-RG <lrojas26@alumnos.uaq.mx>
2023-09-13 14:42:44 -0600

    EStimación R0 e incidencia TODOS LOS DATOS

--- 02_Scripts/12_R0_estimacion.R
+++ 02_Scripts/12_R0_estimacion.R
@@ -10,6 +10,7 @@ head(casos_positivos_re_conteo)
 # Se cargan las funciones
 source("02_Scripts/Functions/Functions.R")
 
+# DATOS AL INICIO
 # Visucalización de datos
 incidencia <- casos_positivos_re_conteo[1:80, -3]
 incidencia <- as.data.frame(incidencia)
@@ -32,3 +33,25 @@ plot(res_parametric_si, legend = FALSE)
 #plot(res_parametric_si, legend = FALSE)
 #dev.off()
 
+# TODOS LOS DATOS
+# Visucalización de datos
+incidencia_all <- casos_positivos_re_conteo[, -3]
+incidencia_all <- as.data.frame(incidencia_all)
+colnames(incidencia_all) <- c("date", "I")
+head(incidencia_all)
+#jpeg("03_Out/Plots/incidencia_covid_qro_all.jpeg", width = 395, height = 285, res = 300, units = "mm")
+plot(as.incidence(incidencia_all$I))
+#dev.off()
+# EpiEstim R0
+res_parametric_si_all <- estimate_R(incidencia_all, 
+                                method="parametric_si",
+                                config = make_config(list(
+                                  mean_si = 4.0, 
+                                  std_si = 1.5))
+)
+
+head(res_parametric_si_all$R)
+plot(res_parametric_si_all, legend = FALSE)
+#jpeg("03_Out/Plots/r0_epiestim_all.jpeg", width = 365, height = 265, res = 300, units = "mm")
+#plot(res_parametric_si, legend = FALSE)
+#dev.off()
