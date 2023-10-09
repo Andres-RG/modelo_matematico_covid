65c939f1f6c2669cc3278b792303c158d1990441
Andres-RG <lrojas26@alumnos.uaq.mx>
2023-10-09 15:22:03 -0600

    dotplot casos positivos

--- 02_Scripts/01_Grafica_de_casos_positivos.R
+++ 02_Scripts/01_Grafica_de_casos_positivos.R
@@ -16,29 +16,34 @@ source("02_Scripts/Functions/Functions.R")
 load("01_RawData/datos_covid_qro.RData")
 load("03_Out/OutData/casos_totales_rangos_edades.RData")
 load("03_Out/OutData/casos_positivos_rangos_edades.RData")
+load("03_Out/OutData/conteo_casos_positivos_rango_edad.RData")
+load("03_Out/OutData/casos_positivos_x_dia_rango_edad.RData")
+load("03_Out/OutData/casos_solo_fecha.RData")
 
 # De la base de datos inicial, se determinaran los rangos de edad de todos
-# los casos positivos a Covid-19 hasta diciembre, 2021. A partir de esta 
+# los casos positivos a Covid-19 hasta 17 de octubre, 2021. A partir de esta 
 # adicion de datos, se construye la grafica para casos positivos divididos
-# por rangos de edades. 
+# por rangos de edades.
 
+# Procesamiento Inicial de Datos:-----------
 # Este objeto contendra todos los casos, con la columna de rango de edades, y
-# clasificados por estos mismos.
-casos_totales_re <- mutate(datos_covid_qro, rango_de_edad = rangos_edades(datos_covid_qro$EDAD))
+# clasificados por estos mismos. Se hace uso de una función desarrollada.
+# casos_totales_re <- mutate(datos_covid_qro, rango_de_edad = rangos_edades(datos_covid_qro$EDAD))
 
 # Se guarda el objeto como un objeto .RData
 # save(casos_totales_re, file = "03_Out/OutData/casos_totales_rangos_edades.RData")
 
 # Este objeto contendra los casos unicamente positivos, de acuerdo a la clasificacion
-# reportada.  
-casos_positivos_re <- filter(casos_totales_re, CLASIFICACION_FINAL == 1 | 
-                        CLASIFICACION_FINAL == 2 |
-                        CLASIFICACION_FINAL == 3 )
+# reportada.
+casos_positivos_re <- filter(casos_totales_re, CLASIFICACION_FINAL == 1 |
+                                 CLASIFICACION_FINAL == 2 |
+                                 CLASIFICACION_FINAL == 3 )
 
 # Se guarda el objeto como un objeto .RData
 # save(casos_positivos_re, file = "03_Out/OutData/casos_positivos_rangos_edades.RData")
 
-# Se elabora la grafica de casos positivos separados por rango de edades
+# GRAFICAS --------------
+## STACK =====
 plot_positivos_re <- ggplot(casos_positivos_re, 
                             aes(x=FECHA_SINTOMAS, fill = rango_de_edad)) + 
     geom_bar(position="stack", stat="count") + 
@@ -56,5 +61,45 @@ plot_positivos_re
 #     width = 365, height = 265, res = 300, units = "mm")
 plot_positivos_re
 #dev.off()
+
+## DOTPLOT =====
+plot_pos_dot <- ggplot(casos_positivos_x_dia_re,
+                       aes(x = FECHA_SINTOMAS,
+                           y = casos_totales,
+                           col = rango_de_edad,
+                           shape = rango_de_edad)) +
+    geom_point(size = 1.5, stroke = 1) +
+    labs(x = "Tiempo", y = "Casos positivos",
+         title = "Casos positivos a COVID-19",
+         col = "Rango de edad", shape = "Rango de edad") +
+    scale_x_date(date_labels = "%b %Y", date_breaks = "1 month") +
+    scale_shape_manual(values = c("18-" = 1,
+                                  "18-29" = 2,
+                                  "30-39" = 3,
+                                  "40-49" = 4,
+                                  "50-59" = 5,
+                                  "60-69" = 6,
+                                  "70+" = 7)) +
+    theme(
+        plot.title = element_text(size = 14, hjust = 0.5),
+        axis.text.x = element_text(size = 7,angle = 45, hjust = 1),
+        axis.line = element_line(colour = "black", size = 0.75),
+        # leyenda
+        legend.position = "right",  # Posición de la leyenda
+        legend.title = element_text(size = 10),  # Título de la leyenda
+        legend.text = element_text(size = 10),  # Texto de la leyenda
+        legend.spacing = unit(0.5, "cm"),
+        # background
+        panel.grid.major.y = element_line(color = "black", size = 0.5, 
+                                          linetype = 2),  # Líneas horizontales al eje x
+        panel.grid.minor = element_blank(),  # Eliminar líneas de cuadrícula menores
+        panel.background = element_blank(),
+        panel.grid.major.x = element_blank()
+    )
+
+plot_pos_dot
+
+#ggsave("03_Out/Plots/plot_casos_positivos_dotplot.jpeg",
+#       plot = plot_pos_dot, width = 2287, height = 1464, units = "px")
 
 
