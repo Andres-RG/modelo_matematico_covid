32a4c9380a8cf25597d58fc92e8a8cb61ac697d0
Andres-RG <lrojas26@alumnos.uaq.mx>
2023-10-25 10:28:03 -0600

    graficas

--- 02_Scripts/15_grafica_modelo_vs_datos.R
+++ 02_Scripts/15_grafica_modelo_vs_datos.R
@@ -7,27 +7,6 @@ head(casos_x_grupos_corte)
 head(muertes_x_grupos_corte)
 
 ##---- CASOS
-fecha_especifica <- as.Date("2021-04-07")
-casos_x_grupos_corte_fecha <- casos_x_grupos_corte  %>%
-    filter(FECHA_SINTOMAS <= fecha_especifica)
-
-fechas_continuas <- seq(min(casos_x_grupos_corte_fecha$FECHA_SINTOMAS), 
-                        max(casos_x_grupos_corte_fecha$FECHA_SINTOMAS), by = "1 day")
-completo_casos_x_grupos_corte <- merge(
-    data.frame(FECHA_SINTOMAS = fechas_continuas),
-    casos_x_grupos_corte, 
-    by = "FECHA_SINTOMAS", all.x = TRUE)
-
-completo_casos_x_grupos_corte$casos_totales[is.na(completo_casos_x_grupos_corte$casos_totales)] <- 0
-
-head(completo_casos_x_grupos_corte)
-
-fechas <- unique(completo_casos_x_grupos_corte$FECHA_SINTOMAS)
-
-fechas_beta_t_out <- mutate(beta_t_out, fecha_sintomas = fechas)
-
-
-
 beta_t_out <-as.data.frame(ode(y = state,
                                times=times,
                                func  = beta_t_modelo_covid,
@@ -43,6 +22,57 @@ prueba2<-data.frame(grupos=rep(c("I1","I2","I3","I4"),
 concatenated_tibble<-bind_rows(casos_x_grupos,prueba2)
 
 
+
+
+
+
+
+
+# Cambiar la base de datos y las variables
+beta_t_out <- as.data.frame(ode(y = state,
+                                times = times,
+                                func = beta_t_modelo_covid,
+                                parms = parameters))
+beta_t_out <- cbind.data.frame(days = seq(as.Date("2020/3/1"), as.Date("2021/4/2"), "days"), beta_t_out)
+beta_t_out <- as_tibble(beta_t_out)
+beta_t_out %>% select(c(days), M1, M2, M3, M4) -> prueba
+prueba2 <- data.frame(grupos = rep(c("M1", "M2", "M3", "M4"), each = 398),
+                      FECHA_SINTOMAS = rep(prueba$days, 4),
+                      casos_totales = c(prueba$M1, prueba$M2, prueba$M3, prueba$M4))
+concatenated_tibble <- bind_rows(muertes_x_grupos, prueba2)
+
+data_frame_filtrado <- concatenated_tibble %>%
+    filter(FECHA_SINTOMAS <= as.Date("2021-04-07"))
+
+casos_solo_filt <- data_frame_filtrado[1:1428,]
+modelo_solo_filt <- data_frame_filtrado[1429:3020,]
+
+combined_data <- rbind(casos_solo_filt, modelo_solo_filt)
+
+colores <- c("#00BFFF", "#FFB90F", "#7CCD7C", "#6A5ACD")
+modelo_vs_casos <- ggplot(combined_data, 
+                          aes(x = FECHA_SINTOMAS)) +
+    geom_line(data = modelo_solo_filt, 
+              aes(x = FECHA_SINTOMAS, y = casos_totales, color = grupos),
+              size = 1) +
+    geom_point(data = casos_solo_filt, 
+               aes(x = FECHA_SINTOMAS, y = casos_totales, 
+                   color = grupos, shape = grupos)) +
+    labs(x = "Fecha de Síntomas", y = "Casos Totales", color = "Grupos", 
+         title = "Modelo vs Casos observados", shape = "Grupos") +
+    theme(axis.line = element_line(colour = "black", size = 0.6),
+          plot.title = element_text(hjust = 0.5, size = 13, face = "bold"),
+          legend.text = element_text(size = 11),
+          legend.title = element_text(size = 11, face = "bold")) +
+    scale_color_manual(values = c("Menores de 18 años" = colores[1],
+                                  "18-39 años" = colores[2],
+                                  "40-59 años" = colores[3],
+                                  "Mayores de 60 años" = colores[4],
+                                  "M1" = colores[1],
+                                  "M2" = colores[2],
+                                  "M3" = colores[3],
+                                  "M4" = colores[4]))
+
 data_frame_filtrado <- concatenated_tibble %>%
     filter(FECHA_SINTOMAS <= as.Date("2021-04-07"))
 
@@ -51,28 +81,138 @@ casos_solo_filt <- data_frame_filtrado[1:1428,]
 modelo_solo_filt <- data_frame_filtrado[1429:3020,]
 
 
-ggplot(aes(x = FECHA_SINTOMAS)) +
-    geom_line(data = casos_solo_filt,
-              aes(y = casos_totales, color = grupos)) +
-    geom_point(data = modelo_solo_filt,
-               aes(y = casos_totales, color = grupos)) +
-    labs(x = "Fecha_sintomas", y = "casos_totales", color = "Grupos") +
-    theme_minimal()
+combined_data <- rbind(casos_solo_filt, modelo_solo_filt)
 
 
+colores <- c("#00BFFF", "#FFB90F", "#7CCD7C", "#6A5ACD")
+modelo_vs_casos <- ggplot(combined_data, 
+       aes(x = FECHA_SINTOMAS)) +
+    ###
+    geom_line(data = modelo_solo_filt, 
+              aes(x = FECHA_SINTOMAS, y = casos_totales, color = grupos),
+              size = 1) +
+    ###
+    geom_point(data = casos_solo_filt, 
+               aes(x = FECHA_SINTOMAS, y = casos_totales, 
+                   color = grupos, shape = grupos)) +
+    labs(x = "Fecha de Síntomas", y = "Casos Totales", color = "Grupos", 
+         title = "Modelo vs Casos observados", shape = "Grupos") +
+    theme(axis.line = element_line(colour = "black", size = 0.6),
+          plot.title = element_text(hjust = 0.5, size = 13, face = "bold"),
+          legend.text = element_text(size = 11),
+          legend.title = element_text(size = 11, face = "bold")) +
+    scale_color_manual(values = c("Menores de 18 años" = colores[1],
+                                  "18-39 años" = colores[2],
+                                  "40-59 años" = colores[3],
+                                  "Mayores de 60 años" = colores[4],
+                                  "I1" = colores[1],
+                                  "I2" = colores[2],
+                                  "I3" = colores[3],
+                                  "I4" = colores[4]))
+#ggsave("03_Out/Plots/modelo_vs_casos.jpeg", plot = modelo_vs_casos, 
+#       width = 3187, height = 1791,units = "px")
+
+
+
+
+##MUERTES------
+beta_t_out <-as.data.frame(ode(y = state,
+                               times=times,
+                               func  = beta_t_modelo_covid,
+                               parms = parameters))
+beta_t_out<-cbind.data.frame(days=seq(as.Date("2020/3/28"),as.Date("2021/4/29"),
+                                      "days"),beta_t_out)
+beta_t_out<-as_tibble(beta_t_out)
+beta_t_out %>% select(c(days),M1,M2,M3,M4)-> prueba
+prueba2<-data.frame(grupos=rep(c("M1","M2","M3","M4"),each=398),
+                    FECHA_MUERTE=rep(prueba$days,4),
+                    muertes_totales=c(prueba$M1,prueba$M2,prueba$M3,prueba$M4))
+concatenated_tibble<-bind_rows(muertes_x_grupos,prueba2)
+
+
+data_frame_filtrado <- concatenated_tibble %>%
+    filter(FECHA_MUERTE <= as.Date("2021-04-29"))
+
+
+casos_solo_filt <- data_frame_filtrado[1:1428,]
+modelo_solo_filt <- data_frame_filtrado[1429:3020,]
 
 
 combined_data <- rbind(casos_solo_filt, modelo_solo_filt)
 
-# Crea el gráfico utilizando ggplot
-ggplot(combined_data, 
-       aes(x = FECHA_SINTOMAS, y = casos_totales)) +
+
+colores <- c("#00BFFF", "#FFB90F", "#7CCD7C", "#6A5ACD")
+casos_solo_filt$color <- rep(colores[1:4], each = 357)  # Asigna los colores a "M1", "M2", "M3", "M4"
+casos_solo_filt$shape <- rep(1:4, each = 357)  # Asigna las formas a "M1", "M2", "M3", "M4"
+
+modelo_vs_muertes <- ggplot(combined_data, 
+                          aes(x = FECHA_MUERTE)) +
+    ###
     geom_line(data = modelo_solo_filt, 
-              aes(x = FECHA_SINTOMAS, y = casos_totales, color = grupos)) +
+              aes(x = FECHA_MUERTE, y = muertes_totales, color = grupos),
+              size = 1) +
+    ###
     geom_point(data = casos_solo_filt, 
-               aes(x = FECHA_SINTOMAS, y = casos_totales, color = grupos)) +
-    labs(x = "Fecha de Síntomas", y = "Casos Totales", color = "Grupos")+ 
-    theme_minimal()
+               aes(x = FECHA_MUERTE, y = muertes_totales, 
+                   color = factor(color), shape = factor(shape))) +
+    labs(x = "Fecha Muerte", y = "Muertes Totales", color = "Grupos", 
+         title = "Modelo vs Muertes observadas", shape = "Grupos") +
+    theme(axis.line = element_line(colour = "black", size = 0.6),
+          plot.title = element_text(hjust = 0.5, size = 13, face = "bold"),
+          legend.text = element_text(size = 11),
+          legend.title = element_text(size = 11, face = "bold"))
+#ggsave("03_Out/Plots/modelo_vs_casos.jpeg", plot = modelo_vs_casos, 
+#       width = 3187, height = 1791,units = "px")
 
 
 
+
+
+
+
+
+
+# Cambiar la base de datos y las variables
+beta_t_out <- as.data.frame(ode(y = state,
+                                times = times,
+                                func = beta_t_modelo_covid,
+                                parms = parameters))
+beta_t_out <- cbind.data.frame(days = seq(as.Date("2020/3/1"), as.Date("2021/4/2"), "days"), beta_t_out)
+beta_t_out <- as_tibble(beta_t_out)
+beta_t_out %>% select(c(days), M1, M2, M3, M4) -> prueba
+prueba2 <- data.frame(grupos = rep(c("M1", "M2", "M3", "M4"), each = 398),
+                      FECHA_SINTOMAS = rep(prueba$days, 4),
+                      casos_totales = c(prueba$M1, prueba$M2, prueba$M3, prueba$M4))
+concatenated_tibble <- bind_rows(muertes_x_grupos, prueba2)
+
+data_frame_filtrado <- concatenated_tibble %>%
+    filter(FECHA_SINTOMAS <= as.Date("2021-04-07"))
+
+casos_solo_filt <- data_frame_filtrado[1:1428,]
+modelo_solo_filt <- data_frame_filtrado[1429:3020,]
+
+combined_data <- rbind(casos_solo_filt, modelo_solo_filt)
+
+colores <- c("#00BFFF", "#FFB90F", "#7CCD7C", "#6A5ACD")
+modelo_vs_casos <- ggplot(combined_data, 
+                          aes(x = FECHA_SINTOMAS)) +
+    geom_line(data = modelo_solo_filt, 
+              aes(x = FECHA_SINTOMAS, y = casos_totales, color = grupos),
+              size = 1) +
+    geom_point(data = casos_solo_filt, 
+               aes(x = FECHA_SINTOMAS, y = casos_totales, 
+                   color = grupos, shape = grupos)) +
+    labs(x = "Fecha de Síntomas", y = "Casos Totales", color = "Grupos", 
+         title = "Modelo vs Casos observados", shape = "Grupos") +
+    theme(axis.line = element_line(colour = "black", size = 0.6),
+          plot.title = element_text(hjust = 0.5, size = 13, face = "bold"),
+          legend.text = element_text(size = 11),
+          legend.title = element_text(size = 11, face = "bold")) +
+    scale_color_manual(values = c("Menores de 18 años" = colores[1],
+                                  "18-39 años" = colores[2],
+                                  "40-59 años" = colores[3],
+                                  "Mayores de 60 años" = colores[4],
+                                  "M1" = colores[1],
+                                  "M2" = colores[2],
+                                  "M3" = colores[3],
+                                  "M4" = colores[4]))
