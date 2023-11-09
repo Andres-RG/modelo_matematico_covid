27c9fd27a8d5654fb6561245521ee3c6eca7dffc
Andres-RG <lrojas26@alumnos.uaq.mx>
2023-11-09 10:16:05 -0600

    actualizacion graficas

--- 02_Scripts/01_Grafica_de_casos_positivos.R
+++ 02_Scripts/01_Grafica_de_casos_positivos.R
@@ -25,16 +25,17 @@ load("03_Out/OutData/casos_solo_fecha.RData")
 # adicion de datos, se construye la grafica para casos positivos divididos
 # por rangos de edades.
 
-# Procesamiento Inicial de Datos:-----------
-# Este objeto contendra todos los casos, con la columna de rango de edades, y
-# clasificados por estos mismos. Se hace uso de una función desarrollada.
+# Procesamiento Inicial de Datos:===============================================
+# Este objeto contendra todos los casos, con la columna de rango de edades, de
+# cada individuo. Se hace uso de una función desarrollada.
 # casos_totales_re <- mutate(datos_covid_qro, rango_de_edad = rangos_edades(datos_covid_qro$EDAD))
 
 # Se guarda el objeto como un objeto .RData
 # save(casos_totales_re, file = "03_Out/OutData/casos_totales_rangos_edades.RData")
 
-# Este objeto contendra los casos unicamente positivos, de acuerdo a la clasificacion
-# reportada.
+# Este objeto contendra los casos unicamente positivos, de acuerdo a la
+# clasificacion reportada.
+
 casos_positivos_re <- filter(casos_totales_re, CLASIFICACION_FINAL == 1 |
                                  CLASIFICACION_FINAL == 2 |
                                  CLASIFICACION_FINAL == 3 )
@@ -42,36 +43,40 @@ casos_positivos_re <- filter(casos_totales_re, CLASIFICACION_FINAL == 1 |
 # Se guarda el objeto como un objeto .RData
 # save(casos_positivos_re, file = "03_Out/OutData/casos_positivos_rangos_edades.RData")
 
-# GRAFICAS --------------
-## STACK =====
-plot_positivos_re <- ggplot(casos_positivos_re, 
-                            aes(x=FECHA_SINTOMAS, fill = rango_de_edad)) + 
-    geom_bar(position="stack", stat="count") + 
-    ggtitle("Casos positivos a COVID por rangos de edades 
-          para el estado de Queretaro") + 
-    labs(x="Tiempo", y="Casos") +
-    labs(fill="Rangos de Edad") +
-    theme(plot.title = element_text(hjust = 0.5))+
-    theme(panel.background = element_rect(fill = "white"), 
+# GRAFICAS======================================================================
+## STACK --------------------------------------------------
+plot_positivos_re <- ggplot(casos_positivos_re,
+                            aes(x=FECHA_SINTOMAS,
+                                fill = rango_de_edad)) + 
+    geom_bar(position="stack",
+             stat="count") +
+    ggtitle("Casos positivos a COVID por rangos de edades
+            para el estado de Queretaro") + 
+    labs( x = "Tiempo",
+          y = "Casos",
+          fill = "Rangos de Edad") +
+    theme(plot.title = element_text(hjust = 0.5),
+          panel.background = element_rect(fill = "gray87"), 
           axis.line = element_line(colour = "black", size = 1)) +
     scale_fill_viridis(discrete = T)
 plot_positivos_re
 # El objeto que contiene la grafica se guarda como una imagen jpeg
-#jpeg("03_Out/Plots/Casos positivos a COVID por rangos de edades para el estado de Queretaro.jpeg",
-#     width = 365, height = 265, res = 300, units = "mm")
-plot_positivos_re
-#dev.off()
+ggsave("03_Out/Plots/cas.jpeg",
+       plot = plot_positivos_re, width = 5733, height = 4300, units = "px")
 
-## DOTPLOT =====
+## DOTPLOT-------------------------------------------------
 plot_pos_dot <- ggplot(casos_positivos_x_dia_re,
                        aes(x = FECHA_SINTOMAS,
                            y = casos_totales,
                            col = rango_de_edad,
                            shape = rango_de_edad)) +
-    geom_point(size = 1.5, stroke = 1) +
-    labs(x = "Tiempo", y = "Casos positivos",
+    geom_point(size = 1.5,
+               stroke = 1) +
+    labs(x = "Tiempo",
+         y = "Casos positivos",
          title = "Casos positivos a COVID-19",
-         col = "Rango de edad", shape = "Rango de edad") +
+         col = "Rango de edad",
+         shape = "Rango de edad") +
     scale_x_date(date_labels = "%b %Y", date_breaks = "1 month") +
     scale_shape_manual(values = c("18-" = 1,
                                   "18-29" = 2,
@@ -99,7 +104,7 @@ plot_pos_dot <- ggplot(casos_positivos_x_dia_re,
 
 plot_pos_dot
 
-#ggsave("03_Out/Plots/plot_casos_positivos_dotplot.jpeg",
+# ggsave("03_Out/Plots/plot_casos_positivos_dotplot.jpeg",
 #       plot = plot_pos_dot, width = 2287, height = 1464, units = "px")
 
 ##FILL | PROPORCION =======
