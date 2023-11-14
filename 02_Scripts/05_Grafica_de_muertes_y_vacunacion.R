8ee16dbc4db7e323b2df35e955d96caf20c7c23f
Andres-RG <lrojas26@alumnos.uaq.mx>
2023-11-14 17:43:36 -0600

    Grafica de muertes y recuperados en etapas de vacunación

--- 02_Scripts/05_Grafica_de_muertes_y_vacunacion.R
+++ 02_Scripts/05_Grafica_de_muertes_y_vacunacion.R
@@ -29,8 +29,8 @@ plot_positivos_muertes_y_no_raw <- ggplot(casos_por_fecha,
                                           group = interaction(rango_de_edad, 
                                                               muerte),
                                           col = muerte)) +
-    geom_line(aes(y = NumCasos), size = 0.5, alpha = 0.75) +
-    geom_smooth(aes(y = NumCasos, linetype = muerte), se = FALSE,
+    geom_line(aes(y = NumCasos), size = 0.5, alpha = 0.6) +
+    geom_smooth(aes(y = NumCasos, linetype = "dashed"), se = FALSE,
             size = 0.9, alpha = 1.5, show.legend = F) +
     facet_grid(rango_de_edad ~ ., scales = "free_y") +
     labs(title = "Comparación de casos de muertes vs recuperados de COVID-19",
@@ -69,7 +69,7 @@ plot_positivos_muertes_y_no_nom <- ggplot(casos_por_fecha,
                                       aes(x = FECHA_SINTOMAS,
                                           group = interaction(rango_de_edad, muerte),
                                           col = muerte)) +
-    geom_line(aes(y = NumCasos / max_cases), size = 0.5, alpha = 0.75) +
+    geom_line(aes(y = NumCasos / max_cases), size = 0.5, alpha = 0.6) +
     geom_smooth(aes(y = NumCasos / max_cases, linetype = muerte), se = FALSE,
                 size = 0.9, alpha = 1.5, show.legend = F) +
     facet_grid(rango_de_edad ~ ., scales = "free_y") +
@@ -117,9 +117,23 @@ fase_1 <- as.Date(c("2020-12-01", "2021-01-31"))
 fase_2 <- as.Date(c("2021-02-01", "2021-04-30"))
 fase_3 <- as.Date(c("2021-05-01", "2021-05-31"))
 fase_4 <- as.Date(c("2021-06-01", "2021-06-30"))
-fase_5 <- as.Date(c("2021-07-01", "2022-03-31"))
+fase_5 <- as.Date(c("2021-07-01", "2021-10-17"))
 
-colores_fases <- c("lightblue", "lightgreen", "lightpink", "lightyellow", "lightcoral")
+fase_1_coords <- data.frame(xmin = fase_1[1],
+                            xmax = fase_1[2],
+                            ymin = -Inf, ymax = Inf)
+fase_2_coords <- data.frame(xmin = fase_2[1],
+                            xmax = fase_2[2],
+                            ymin = -Inf, ymax = Inf)
+fase_3_coords <- data.frame(xmin = fase_3[1],
+                            xmax = fase_3[2],
+                            ymin = -Inf, ymax = Inf)
+fase_4_coords <- data.frame(xmin = fase_4[1],
+                            xmax = fase_4[2],
+                            ymin = -Inf, ymax = Inf)
+fase_5_coords <- data.frame(xmin = fase_5[1],
+                            xmax = fase_5[2],
+                            ymin = -Inf, ymax = Inf)
 
 casos_por_fecha_vac$FECHA_SINTOMAS <- as.Date(casos_por_fecha_vac$FECHA_SINTOMAS)
 
@@ -128,11 +142,11 @@ plot_muertes_vac <- ggplot(casos_por_fecha_vac,
                                group = interaction(rango_de_edad,
                                                    muerte),
                                col = muerte)) +
-  geom_line(aes(y = NumCasos), size = 0.5, alpha = 0.75) +
-  geom_smooth(aes(y = NumCasos, linetype = muerte), se = FALSE,
+  geom_line(aes(y = NumCasos), size = 0.5, alpha = 0.6) +
+  geom_smooth(aes(y = NumCasos, linetype = "dashed"), se = FALSE,
               size = 0.9, alpha = 1.5, show.legend = F) +
   facet_grid(rango_de_edad ~ ., scales = "free_y") +
-  labs(title = "Comparación de casos de muertes vs recuperados de COVID-19",
+  labs(title = "Comparación de casos de muertes y recuperados en fases de vacunación",
        x = "Tiempo", y = "No. de casos", fill = "Muerte") +
   theme(
     plot.title = element_text(size = 14, hjust = 0.5),
@@ -147,15 +161,31 @@ plot_muertes_vac <- ggplot(casos_por_fecha_vac,
   ) +
   scale_x_date(date_labels = "%b %Y", date_breaks = "1 month") + #agrega los meses
   scale_y_continuous(labels = scales::comma) + #agrega el numero de casos
-  scale_fill_manual(values = c("Muerte" = "red4", "No muerte" = "dodgerblue"),
+  scale_fill_manual(values = c("Muerte" = "firebrick3", "No muerte" = "deepskyblue"),
                     labels = c("Fallecimientos", "Recuperados")) +
-  scale_color_manual(values = c("Muerte" = "red4", "No muerte" = "dodgerblue"),
+  scale_color_manual(values = c("Muerte" = "firebrick3", "No muerte" = "deepskyblue"),
                      labels = c("Fallecimientos", "Recuperados")) + 
   scale_linetype_manual(values = c("dashed", "solid")) +
-  geom_rect(data = data.frame(xmin = fase_1[1], xmax = fase_1[2]),
-            aes(xmin = as.numeric(xmin) - 0.5, xmax = as.numeric(xmax) + 0.5,
-                ymin = -Inf, ymax = Inf),
-            fill = colores_fases[1], alpha = 0.5, inherit.aes = FALSE)
+  geom_rect(data = fase_1_coords,
+            aes(xmin = xmin, xmax = xmax, ymin = -Inf, ymax = Inf),
+            fill = "lightcoral", alpha = 0.5, inherit.aes = FALSE) + 
+  geom_rect(data = fase_2_coords,
+            aes(xmin = xmin, xmax = xmax, ymin = -Inf, ymax = Inf),
+            fill = "tan1", alpha = 0.5, inherit.aes = FALSE) +
+  geom_rect(data = fase_3_coords,
+            aes(xmin = xmin, xmax = xmax, ymin = -Inf, ymax = Inf),
+            fill = "gold", alpha = 0.5, inherit.aes = FALSE) +
+  geom_rect(data = fase_4_coords,
+            aes(xmin = xmin, xmax = xmax, ymin = -Inf, ymax = Inf),
+            fill = "lightgreen", alpha = 0.5, inherit.aes = FALSE) +
+  geom_rect(data = fase_5_coords,
+            aes(xmin = xmin, xmax = xmax, ymin = -Inf, ymax = Inf),
+            fill = "lightblue", alpha = 0.5, inherit.aes = FALSE)
 
 # El objeto se guarda como un objeto jpeg
 plot_muertes_vac
+
+jpeg("03_Out/Plots/grafica_muertes_recuperados_etapas_vacunacion.jpeg",
+    width = 365, height = 265, res = 300, units = "mm")
+plot_muertes_vac
+dev.off()
