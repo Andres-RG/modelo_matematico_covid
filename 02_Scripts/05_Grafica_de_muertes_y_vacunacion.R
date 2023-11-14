16267a439d1e79e42dd0cce9004c7c03bc1aaafb
Andres-RG <lrojas26@alumnos.uaq.mx>
2023-11-14 15:33:46 -0600

    casos con vacunacin

--- 02_Scripts/05_Grafica_de_muertes_y_vacunacion.R
+++ 02_Scripts/05_Grafica_de_muertes_y_vacunacion.R
@@ -18,6 +18,7 @@ load("03_Out/OutData/conteo_casos_muerte.RData")
 load("03_Out/OutData/datos_muerte.RData")
 load("03_Out/OutData/muertes_datos_x_grupos.RData")
 load("03_Out/OutData/casos_solo_fecha.RData")
+load("03_Out/OutData/casos_positivos_re_m_vac.RData")
 
 # Grafica muertes vs recuperados Datos crudos ==================================
 # Se genera una grafica donde se observan las fechas por meses, de los casos 
@@ -99,54 +100,10 @@ plot_positivos_muertes_y_no_nom
 # plot_positivos_muertes_y_no_nom
 # dev.off()
 
-
+# Grafica vacunacion vs muertes ================================================
 # Se va a generar una grafica donde se observen las muertes de todos los casos 
 # positivos, separado por estructura de edad y donde se observa ademas la fecha
 # correspondiente al esquema de vacunacion en curso. 
 
-# Fechas de vacunacion, de acuerdo a la pag de la Secretaria de Salud
-# Diciembre 2020 - Febrero 2021 : Personal de salud === PRIMERA FASE
-# Febrero - Mayo 2021 : 60+                         === SEGUNDA FASE
-# Mayo - Junio 2021 : 50 - 59                       === TERCERA FASE
-# Junio - Julio 2021 : 40 - 49                      === CUARTA FASE
-# Julio 2021 - Marzo 2022 : resto                   === QUINTA FASE
 
-# A la base de datos de casos_solo_positivos_muerte_re se le va a gregar la 
-# columna de las fechas de vacunacion. Para esto, se usa la funcion 
-# fechas_vacunacion, que agrega la etapa de vacunacion de acuerdo a la fecha 
-# de inicio de sintomas.
-vacunacion <- fechas_vacunacion(casos_solo_positivos_muerte_re$FECHA_SINTOMAS)
-casos_solo_positivos_muerte_re <- mutate(casos_solo_positivos_muerte_re, 
-                                         FECHAS_VACUNACION = vacunacion)
-# Se guarda el nuevo objeto como un objeto .RData
-# save(casos_solo_positivos_muerte_re, 
-#     file = "03_Out/OutData/casos_positivos_re_m_vac.RData")
-load("03_Out/OutData/casos_positivos_re_m_vac.RData")
-
-plot_positivos_m_vacunacion <- ggplot(casos_solo_positivos_muerte_re, 
-                                      aes(x = FECHA_SINTOMAS, 
-                                          y = rango_de_edad, 
-                                          col = Muerte, 
-                                          fill = FECHAS_VACUNACION)) +
-    geom_density_ridges2(alpha = 0.5, size = 1) +
-    theme(plot.title = element_text(hjust = 0.5))+
-    theme(panel.background = element_rect(fill = "white"), 
-          axis.line = element_line(colour = "black", size = 1)) +
-    scale_x_date(date_breaks = "1 month", date_labels = "%b") +
-    labs(y = "Rangos de edades", x = "Tiempo") +
-    ggtitle("Densidad de casos positivos que murieron y que no muerieron con las fechas de vacunación")
-# Se guarda el objeto como un png
-#png("03_Out/Plots/Grafica de muertes de casos positivos x fechas de vacunacion.png", 
-#    width = 550, height = 350)
-plot_positivos_m_vacunacion
-dev.off()
-
-
-
-#
-plot_positivos_muertes_y_no <- ggplot(casos_solo_positivos_muerte_re, 
-                                      aes(x = FECHA_SINTOMAS,
-                                          fill = Muerte)) +
-    geom_bar(stat = "fill")
-plot_positivos_muertes_y_no
 
