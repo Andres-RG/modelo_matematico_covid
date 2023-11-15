8a0451c7ef0ae15c08231f1b6083fb308c5d0adc
Andres-RG <lrojas26@alumnos.uaq.mx>
2023-11-15 16:56:18 -0600

    determinación de proporciones de individuos

--- 02_Scripts/07_Porcentajes_reales_vs_modelo.R
+++ 02_Scripts/07_Porcentajes_reales_vs_modelo.R
@@ -6,33 +6,26 @@ library(tidyverse)
 library(viridisLite)
 library(viridis)
 library(deSolve)
-library(ape)
 library(lubridate)
-library(ggmatplot)
-library(randomcoloR)
 
 # Cargar las funciones
 source("02_Scripts/Functions/Functions.R")
 
-
 # Se cargan los scripts del modelo
-source("02_Scripts/06_0_Resolucion_numerica_todos_los_grupos.R")
+source("02_Scripts/05_Resolucion_numerica.R")
 
 # Se cargan los datos del modelo
 load("03_Out/OutData/casos_totales_rangos_edades.RData")
 
-
-
-
-# Probabilidades reales ====
+# Probabilidades reales ========================================================
 totales_reales <- mutate(casos_totales_re, individuo = 1)
-## INFECTADOS
+## INFECTADOS ------------------------------------------------------------------
 infectados_reales <- filter(totales_reales, CLASIFICACION_FINAL == 1 | 
                          CLASIFICACION_FINAL == 2 |
                          CLASIFICACION_FINAL == 3)
 p_infec_real <-  sum(infectados_reales$individuo)/sum(totales_reales$individuo)
 
-## HOSPITALIZADOS
+## HOSPITALIZADOS --------------------------------------------------------------
 hospitalizados_reales <- filter(totales_reales, TIPO_PACIENTE == 2)
 p_hosp_real <- sum(hospitalizados_reales$individuo)/sum(totales_reales$individuo)
 
@@ -40,71 +33,84 @@ p_hosp_real <- sum(hospitalizados_reales$individuo)/sum(totales_reales$individuo
 intubados_reales <- filter(totales_reales, INTUBADO == 1)
 p_int_real <- sum(intubados_reales$individuo)/sum(totales_reales$individuo)
 
-## MUERTES
+## MUERTES ---------------------------------------------------------------------
 muertes_reales <- mutate(totales_reales, muerte = c
                          ( ifelse( !is.na( totales_reales$FECHA_DEF ), 
                                    "Muerte", "No muerte") ))
 muertes_reales <- filter(muertes_reales, muerte == "Muerte")
 p_muerte_real <- sum(muertes_reales$individuo)/sum(totales_reales$individuo)
 
-## TABLA
-porcentajes_reales <- c(p_infec_real, p_hosp_real, p_int_real, p_muerte_real)
+## TABLA -----------------------------------------------------------------------
+porcentajes_reales <- matrix(c(p_infec_real, p_hosp_real, p_int_real, p_muerte_real), 
+                             nrow = 4)
+rownames(porcentajes_reales) <- c("Inf", "Hosp", "Int", "Muer")
+colnames(porcentajes_reales) <- "p"
+porcentajes_reales
 
+# Probabilidades del modelo ====================================================
+# Calcular la proporción de individuos para cada columna
+proporcion_individuos <- beta_t_out[, -1] / rowSums(beta_t_out[, -1])
 
+# Añadir la columna de tiempo
+proporcion_individuos <- cbind(beta_t_out[, 1, drop = FALSE], proporcion_individuos)
 
-# Probabilidades del modelo ====
-## INFECCION
-p_infect_modelo <- sum(c(out_all_groups[,4], out_all_groups[,12],
-                         out_all_groups[,20], out_all_groups[,28]))/sum(
-                             out_all_groups[,2:33]
-                         )
-## HOSPITALIZADOS
-p_hosp_modelo <- sum(c(out_all_groups[,6], out_all_groups[,14],
-                       out_all_groups[,22], out_all_groups[,30]))/sum(
-                           out_all_groups[,2:33]
-                       )
-## INTUBADOS
-p_intu_modelo <- sum(c(out_all_groups[,7], out_all_groups[,15],
-                       out_all_groups[,23], out_all_groups[,31]))/sum(
-                           out_all_groups[2:33]
-                       )
-## MUERTOS
-p_muerte_modelo <- sum(c(out_all_groups[,8], out_all_groups[,16],
-                         out_all_groups[,24], out_all_groups[,32]))/sum(
-                             out_all_groups[2:33]
-                         )
-## TABLA
-porcentajes_modelo <- c(p_infect_modelo, p_hosp_modelo,
-                                   p_intu_modelo, p_muerte_modelo)
+# Imprimir las primeras filas del resultado
+head(proporcion_individuos)
+#INFECTADOS --------------------------------------------------------------------
+# Seleccionar las columnas I1, I2, I3, I4 y la columna de tiempo
+subset_datos_I <- proporcion_individuos[c("time", "I1", "I2", "I3", "I4")]
 
-# TABLA
-porcentajes <- data.frame(porcentajes_reales, porcentajes_modelo)
-rownames(porcentajes) <- c("p_infeccion", "p_hospitalizacion",
-                           "p_intubacion", "p_muerte")
-porcentajes <- t(porcentajes)
+# Sumar las columnas I1, I2, I3, I4
+subset_datos_I$sum_I <- rowSums(subset_datos_I[, c("I1", "I2", "I3", "I4")])
 
+# Filtrar el renglón 398
+I_398 <- subset_datos_I[398, 6]
+#HOSPITALIZADOS ----------------------------------------------------------------
+# Seleccionar las columnas I_h1, I_h2, I_h3, I_h4 y la columna de tiempo
+subset_datos_I_h <- proporcion_individuos[c("time", "I_h1", "I_h2", "I_h3", "I_h4")]
 
+# Sumar las columnas I1, I2, I3, I4
+subset_datos_I_h$sum_I_h <- rowSums(subset_datos_I_h[, c("I_h1", "I_h2", "I_h3", "I_h4")])
 
+# Filtrar el renglón 398
+I_h_398 <- subset_datos_I_h[398, 6]
+#INTUBADOS ---------------------------------------------------------------------
+# Seleccionar las columnas I_h1, I_h2, I_h3, I_h4 y la columna de tiempo
+subset_datos_I_i <- proporcion_individuos[c("time", "I_i1", "I_i2", "I_i3", "I_i4")]
 
+# Sumar las columnas I1, I2, I3, I4
+subset_datos_I_i$sum_I_i <- rowSums(subset_datos_I_i[, c("I_i1", "I_i2", "I_i3", "I_i4")])
 
-porcentajes <- matrix(ncol = 3, nrow = 8)
-porcentajes[1:4, 1] <- porcentajes_modelo
-porcentajes[5:8, 1] <- porcentajes_reales
-porcentajes[1:4, 2] <- c("modelo")
-porcentajes[5:8, 2] <- c("reales")
-porcentajes[,3] <- c("p_infeccion", "p_hospitalizacion",
-                     "p_intubacion", "p_muerte")
-colnames(porcentajes) <- c("p", "tipo", "dato")
-porcentajes <- as.data.frame(porcentajes)
+# Filtrar el renglón 398
+I_i_398 <- subset_datos_I_i[398, 6]
+#MUERTOS -----------------------------------------------------------------------
+# Seleccionar las columnas I_h1, I_h2, I_h3, I_h4 y la columna de tiempo
+subset_datos_M <- proporcion_individuos[c("time", "M1", "M2", "M3", "M4")]
 
-ggplot(porcentajes, aes(fill = tipo, x = dato, y = p)) +
-    geom_bar(position="stack", stat="identity") +
-    labs(y = "Porcentaje", x = "Variable", fill="Datos") +
-    ggtitle("Porcentajes relativos de los datos reales vs inferidos") +
-    theme(plot.title = element_text(hjust = 0.5))+
-    theme(panel.background = element_rect(fill = "white"), 
-          axis.line = element_line(colour = "black", size = 1)) +
-    scale_x_discrete(labels =c("p_hospitalizacion"="Hospitalizados", 
-                               "p_infeccion"="Infectados", 
-                               "p_intubacion"="Intubados",
-                               "p_muerte"="Muerte"))
+# Sumar las columnas I1, I2, I3, I4
+subset_datos_M$sum_M <- rowSums(subset_datos_M[, c("M1", "M2", "M3", "M4")])
+
+# Filtrar el renglón 398
+M_398 <- subset_datos_M[398, 6]
+
+## TABLA -----------------------------------------------------------------------
+pocentajes_modelo <- matrix(c(I_398,I_h_398,I_i_398,M_398), nrow = 4)
+rownames(pocentajes_modelo) <- c("Inf", "Hosp", "Int", "Muer")
+colnames(pocentajes_modelo) <- "p"
+pocentajes_modelo
+
+# datos ========================================================================
+datos <- data.frame(
+  estado = c("Inf", "Hosp", "Int", "Muer"),
+  model = c(1.619934e-05, 2.119885e-06, 4.435176e-06, 1.017782e-03),
+  real = c(0.47295361, 0.09022145, 0.01333372, 0.03219750)
+)
+
+# Graficas =====================================================================
+plot_porcentajes <- ggplot(datos, aes(x = estado)) +
+  geom_bar(aes(y = model, fill = "model"), stat = "identity", position = "stack") +
+  geom_bar(aes(y = real, fill = "real"), stat = "identity", position = "stack") +
+  labs(title = "Comparación entre porcentajes reales con el modelo",
+       x = "Categoría",
+       y = "Valor")
+plot_porcentajes
